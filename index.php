bb95fe1865eb635cf9c31e3cfebc7ed80eb95c63
eNJi25 <nfischer.contact@gmail.com>
2024-10-21 13:19:04 +0200

    feat: add link and script for bootstrap and bootstrap-icons

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/node_modules/bootstrap-icons/font/bootstrap-icons.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Test</h1>
+    <div class="container">
+        <h1>Test mise en page boostrap</h1>
+        <p>Test icone <i class="bi bi-apple"></i></p>
+    </div>
+
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
