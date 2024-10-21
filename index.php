f5d824899ff83357105e684f63a5ecd19272793a
eNJi25 <nfischer.contact@gmail.com>
2024-10-21 13:44:18 +0200

    Finalization of the work environment with sass

--- index.php
+++ index.php
@@ -3,14 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/scss/main.css">
     <title>Document</title>
 </head>
 <body>
     <div class="container">
-        <h1>Test mise en page boostrap</h1>
-        <p>Test icone <i class="bi bi-apple"></i></p>
+        <h1 class="text-primary">Test mise en page boostrap</h1>
+        <p class="bg-secondary">Test icone <i class="bi bi-apple"></i></p>
     </div>
 
     <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
