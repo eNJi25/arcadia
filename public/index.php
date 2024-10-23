a3c66e1764781483faaf44a6c931e8419ef6f35d
eNJi25 <nfischer.contact@gmail.com>
2024-10-23 21:48:39 +0200

    add public directory for index and assets and add footer

---
+++ public/index.php
@@ -0,0 +1,91 @@
+<h1?php
+    require_once '../vendor/autoload.php' ;
+    ?>
+    <!DOCTYPE html>
+    <html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/scss/main.css">
+        <title>Arcadia - Bienvenue</title>
+    </head>
+
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg bg-primary">
+                <div class="container">
+                    <a class="navbar-brand" href="#">ARCADIA</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Accueil</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Habitats</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Avis</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Rapports</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Connexion/Déconnexion</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+            <div class="container-fluid bg-primary p-2">
+                <div class="col-12 text-center color-principal">
+                    <p>Nos horaires</p>
+                    <p>Du Mardi au Dimanche</p>
+                    <p>09:00 - 19:00</p>
+                </div>
+                <div class="row d-flex space-between">
+                    <div class="col-6 text-center">
+                        <p><a href="#">Nous contacter</a></p>
+                        <p><a href="#">Mentions légales</a></p>
+                    </div>
+                    <div class="col-6 text-center">
+                        <p>Nous suivre</p>
+                        <div><a href="#"><i class="bi bi-instagram"> </i></a><a href="#"><i class="bi bi-twitter"> </i></a><a href="#"><i class="bi bi-facebook"></i></a></div>
+                    </div>
+                </div>
+                <div class="col-12 text-center">
+                    <p class="mb-0">Copyright Arcadia 2024 - Tout droits résérvés </p>
+                </div>
+            </div>
+
+        </footer>
+        <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    </body>
+
+    </html>
